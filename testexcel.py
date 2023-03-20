2e85dcc5d203cd99bbd8b1e5c4947a4dae8057cd
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-03-20 14:46:49 +0100

    Update testexcel.py

--- testexcel.py
+++ testexcel.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.title("excel update app")
 df = pd.read_excel("QuestionsFinancedeMarche6.xlsx")
 st.write(df)
+
+dataframe_all = pd.ExcelFile("QuestionsFinancedeMarche6.xlsx")
+dataframe_allsheets = dataframe_all.sheet_names
+st.write(dataframe_allsheets)
+
