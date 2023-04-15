deecd3463ae2ca917dedc382e0d9fcff1ee0f07a
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-04-15 16:57:26 +0200

    Update Streamlitapplication.py

--- Streamlitapplication.py
+++ Streamlitapplication.py
@@ -113,19 +113,19 @@ def treatment(p_dataframe):
     elif current_row_possibilities == "INPUT": #give the possibility to answer by writting
         message = st.text_area("Enter your text : ", "")
         if st.button("Summarize"): 
-#           real_answer = current_row['Answer'].iloc[0]#useless to check 
-#           real_justification = current_row['Justification'].iloc[0]#useless to justification        
+          real_answer = current_row['Answer'].iloc[0]#useless to check 
+          real_justification = current_row['Justification'].iloc[0]#useless to justification        
 
-#           sentences = [
-#               message,
-#               real_answer
-#               ]
-#           sentence_embeddings = model.encode(sentences) #modèle intermédiaire
-#           similitude = util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1]) 
-#           if similitude > 0.5 : 
-#             st.success("Exactement ! Quelques compléments : \n " + real_justification)
-#           else : 
-#             st.error("Faux ! puisque : " + real_justification)
+          sentences = [
+              message,
+              real_answer
+              ]
+          sentence_embeddings = model.encode(sentences) #modèle intermédiaire
+          similitude = util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1]) 
+          if similitude > 0.5 : 
+            st.success("Exactement ! Quelques compléments : \n " + real_justification)
+          else : 
+            st.error("Faux ! puisque : " + real_justification)
             
 #             st_lottie(load_lottiefile("\\\\ad-its.credit-agricole.fr\\dfs\\HOMEDIRS\\AMUNDI\\michoux\\Desktop\\Personnel\\Projets Python\\Questions d’entretiens en Finance de Marché\\versiongithub\\lottiefiles\\hello.json"), speed = 1, reverse=False, loop = True, quality  = "low")
             # tokens = pegasus_tokenizer(message, truncation = True, padding = "longest", return_tensors = "pt")            
