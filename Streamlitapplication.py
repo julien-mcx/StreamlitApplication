ea841a7a71443fbe12f85297cbda2963b4e45f72
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-05-25 11:03:54 +0200

    Update Streamlitapplication.py

--- Streamlitapplication.py
+++ Streamlitapplication.py
@@ -145,8 +145,8 @@ def treatment(p_dataframe):
     
     elif current_row_possibilities == "Image": #give the possibility to answer by writting
         st.write("gg")
-        message = st.write("Choose your answer from the following possibilities", "")
-        img2 = image_select("Label", ["images/Question_6_Options/CallOK.JPG", "images/Question_6_Options/Put.JPG"])
+#         message = st.write("Choose your answer from the following possibilities", "")
+#         img2 = image_select("Label", ["images/Question_6_Options/CallOK.JPG", "images/Question_6_Options/Put.JPG"])
 #         if img == "images/Question_6_Options/Put.JPG" : 
 #             st.write("gg")
 
