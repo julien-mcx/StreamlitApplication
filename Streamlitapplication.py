4396fa90d3ee676d8b819691d6ee78615882d0ea
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-07-01 17:11:44 +0200

    Update Streamlitapplication.py

--- Streamlitapplication.py
+++ Streamlitapplication.py
@@ -152,6 +152,7 @@ def treatment(p_dataframe):
     #         st.write("gg")
     
     elif current_row_possibilities == "Image": #give the possibility to answer by writting
+        current_row_images_possibilities = current_row_images_possibilities.split(", ") #attention à l'espace
         st.write(f"{current_row_images_possibilities.tolist()[0]}")
         test = ["images/Question_6_Options/CallOK.JPG", "images/Question_6_Options/Put.JPG"]
         st.write(f"{test[0]}")
